6ef71df9b01ebdeace69b634ead50c3ce48f8d6f
DanteDercksen <55442624+DanteDercksen@users.noreply.github.com>
2021-09-20 10:12:53 +0200

    Initial commit

---
+++ Tile.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GADE6112_POE_19014267
+{
+    abstract class Tile
+    {
+        public enum TileType { Hero, Enemy, Goblin, Weapon};
+
+        protected int x;
+        protected int y;
+
+        public int X
+        {
+            get { return x; }
+        }
+
+        public int Y
+        {
+            get { return y; }
+        }
+
+        public Tile(int x, int y)
+        {
+            x = X;
+            y = Y;
+        }
+
+
+
+    }
+}
